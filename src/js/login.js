820cf6e55f428f2c6845917b2dd979602ab2101e
Yossueth <patricksarguedas2806@gmail.com>
2024-08-21 16:03:45 -0600

    creacion de 1 parte de pagina de solicitudes

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,4 @@
-import { get } from "../server/fetch";
+import { get } from "../services/fetch";
 
 let loginForm = document.getElementById("loginForm");
 let texto = document.getElementById("texto");
@@ -7,7 +7,7 @@ loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  let userList = (await get()) || [];
+  let userList = await get() || [];
 
   let validUser = userList.find(
     (user) => user.email === email && user.password === password
---
+++ src/js/solicitudes.js
@@ -0,0 +1,37 @@
+import { getDatos, putDatos } from "../services/datos";
+
+let informacion = document.querySelector(".informacion");
+
+dataPc();
+async function dataPc() {
+  let datosPc = await getDatos();
+
+  datosPc.forEach((element) => {
+    let contenedor = document.createElement("div");
+    informacion.appendChild(contenedor);
+
+    let nombre = document.createElement("p");
+    nombre.innerHTML = element.nombre;
+    contenedor.appendChild(nombre);
+
+    let sede = document.createElement("p");
+    sede.innerHTML = element.sede;
+    contenedor.appendChild(sede);
+
+    let fechaSalida = document.createElement("p");
+    fechaSalida.innerHTML = element.fechaSalida;
+    contenedor.appendChild(fechaSalida);
+
+    let fechaEntrega = document.createElement("p");
+    fechaEntrega.innerHTML = element.fechaEntrada;
+    contenedor.appendChild(fechaEntrega);
+
+    let btnAceptar = document.createElement("button");
+    btnAceptar.innerHTML = "Aceptar";
+    contenedor.appendChild(btnAceptar);
+
+    let btnRechazar = document.createElement("button");
+    btnRechazar.innerHTML = "Rechazar";
+    contenedor.appendChild(btnRechazar);
+  });
+}
